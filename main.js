c053a313966efdff2810e268fc0dbb086dcbe9c8
fflorezz <felipeflorezart@gmail.com>
2023-06-09 18:02:42 -0500

    Add data atribute exit q
    31
    
    q
    exit
    
    ·

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 (function(
   window
 ){
-  const element = window.document.querySelector('#my-form')
-  const newElement = window.document.createElement('h1')
-  newElement.innerHTML = 'Hello World'
+  const element = window.document.querySelector('#my-form') 
+  const message = element.getAttribute('data-mi-formulario') ||'Hello World'
+  const newElement = window.document.createElement('h1') 
+  newElement.innerHTML = message
   element.appendChild(newElement)
 })(window)
